e46656d0ccce40eb0ee4109b4b486c9cf3359d1a
KobinaOtc <kobinaotchereadjoku@gmail.com>
2021-08-29 13:07:54 +0000

    progress

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char str1[60] = "\"Programming is like building a multilingual puzzle";
+
+	puts(str1);
+	return (0);
+}
