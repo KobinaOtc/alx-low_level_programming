3bef19faf79c8bd53caaabb440735bdf85e18086
kobinaotch <kobinaotchereadjoku@gmail.com>
2021-09-09 18:26:17 +0000

    Well

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,23 +6,23 @@ char * _strcat - a function that concatenates tow strings
 char *_strcat (char *dest, char *src)
 
 {
-    int i, j;
-    i = 0;
-    j = 0;
+int i, j;
+i = 0;
+j = 0;
 
-    while (*(dest + i) != '\0')
-    {
-        i++;
-        j++;
-    }
+while (*(dest + i) != '\0')
+{
+i++;
+j++;
+}
 
-    i = 0;
-    while (*(src + i) != '\0')
-    {
-        dest[j] = src[i];
-        i++;
-        j++;
-    }
-    dest[j] = '\0';
-    return (dest);
+i = 0;
+while (*(src + i) != '\0')
+{
+dest[j] = src[i];
+i++;
+j++;
+}
+dest[j] = '\0';
+return (dest);
 }
