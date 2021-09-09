7501e53ab482c4a0066a44a610aa260676fc80bc
kobinaotch <kobinaotchereadjoku@gmail.com>
2021-09-09 18:31:25 +0000

    bettyfied

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,21 +1,21 @@
 #include "main.h"
 /**
- * _strcmp - compares two strings, ending at the first byte that is different.
- * @s1: string to be compared
- * @s2: string to compare by
- * Return: the difference between the first character that are not the same
- */
+* _strcmp - compares two strings, ending at the first byte that is different.
+* @s1: string to be compared
+* @s2: string to compare by
+* Return: the difference between the first character that are not the same
+*/
 
- int _strcmp(char *s1, char *s2)
- {
-     int count, comVal;
-     count = 0;
+int _strcmp(char *s1, char *s2)
+{
+int count, comVal;
+count = 0;
 
-     while (s1[count] == s2[count] && s1[count] != '\0')
-     {
-         count++;
-     }
+while (s1[count] == s2[count] && s1[count] != '\0')
+{
+count++;
+}
 
-     comVal = s1[count] - s2[count];
-     return (comVal);
- }
+comVal = s1[count] - s2[count];
+return (comVal);
+}
--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,20 +1,20 @@
 #include "main.h"
 
 /**
- * string_toupper - capitalize all letters in string
- * @s: string to manipulate
- * Return: string with all letters capitalized
- */
+* string_toupper - capitalize all letters in string
+* @s: string to manipulate
+* Return: string with all letters capitalized
+*/
 
- char *string_toupper (char *s)
+char *string_toupper (char *s)
 
- {
-     int i = 0;
+{
+int i = 0;
 
-     for (i = 0; s[i] != '\0'; i++)
-     {
-         if (s[i] >= 'a' && s[i] <= 'z')
-            s[i] = s[i] - 'a' + 'A';
-     }
-     return (s);
- }
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] = s[i] - 'a' + 'A';
+}
+return (s);
+}
