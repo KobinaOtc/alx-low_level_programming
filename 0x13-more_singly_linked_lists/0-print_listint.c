5369d3a683adcbe869048fcc070aac8a40c2cf26
KobinaOtc <kobinaotchereadjoku@gmail.com>
2021-10-12 22:03:57 +0000

    fixed up, advance

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,21 @@
 #include "lists.h"
-#include <stdio.h>
-
 /**
-* print_listint - prints all elements of listint_t list
-* Return: number of nodes
-* @h:  pointer to the list
+* print_listint - prints all elements of a listint_t list.
+* @h: pointer to first element in a list.
+*
+* Return:  number of nodes.
 */
-
 size_t print_listint(const listint_t *h)
 {
-size_t numb_of_nodes = 0;
+int num_of_nodes;
+
+num_of_nodes = 0;
 while (h != NULL)
 {
 printf("%d\n", h->n);
-numb_of_nodes += 1;
 h = h->next;
+num_of_nodes++;
 }
 
-return (numb_of_nodes);
+return (num_of_nodes);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,23 +1,22 @@
 #ifndef LISTS_H
 #define LISTS_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <stdio.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * 
- */
+  * struct listint_s - singly linked list
+  * @n: integer
+  * @next: points to the next node
+  *
+  * Description: singly linked list node structure
+  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
+
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -32,6 +31,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
+listint_t *find_listint_loop(listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 #endif
