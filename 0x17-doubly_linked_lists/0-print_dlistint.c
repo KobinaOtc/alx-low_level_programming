9912585a4677861f92a3e9a02baa2148bc4ec5a2
KobinaOtc <kobinaotchereadjoku@gmail.com>
2021-11-25 13:49:40 +0000

    update algorithm

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,14 +10,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	unsigned int nodes = 0;
-	if (h==NULL)
-		return(0);
-	while (h != NULL)
-	{
-		printf("%d\n", h->n);
-		nodes++;
-		h = h->next;
-	}
-	return(nodes);
-}
+  size_t nodes = 0;
+  while (h)
+    {
+      nodes++;
+      printf("%d\n", h->n);
+      h = h->next;
+    };
+  return (nodes);
+};
