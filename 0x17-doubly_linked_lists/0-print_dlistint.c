93ebfd29a16468612353c19eb48f6628761c25ce
KobinaOtc <kobinaotchereadjoku@gmail.com>
2021-11-25 14:07:53 +0000

    linki time

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,12 +9,12 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t i;
+	size_t i;
 
-  for (i = 0; h != NULL; i++)
-    {
-      printf("%d\n", h->n);
-      h = h->next;
-    }
-  return (i);
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
 }
