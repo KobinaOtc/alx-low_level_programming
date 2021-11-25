58985675d0a7a77e8564aae63b5059a6be8aeeaf
KobinaOtc <kobinaotchereadjoku@gmail.com>
2021-11-25 13:54:35 +0000

    update algorithm

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,21 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
+#include <stdio.h>
 
 /**
- * print_dlistint - prints all elements of a doubly linked list
- * @h: the head of the linked list
+ * print_dlistint - print all the elements of a dlistint_t list
+ * @h: pointer to the start of the linked list
  *
- * Return: the number of nodes
+ * Return: number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-  size_t nodes = 0;
-  while (h)
+  size_t i;
+
+  for (i = 0; h != NULL; i++)
     {
-      nodes++;
       printf("%d\n", h->n);
       h = h->next;
-    };
-  return (nodes);
-};
+    }
+  return (i);
+}
